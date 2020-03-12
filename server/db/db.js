2cdfbf057dd7b4787e18388631aa61b08960e9c2
CJ Jessett <cjessett@gmail.com>
2020-03-11 23:31:31 -0700

    Make database connection configurable to env

--- server/db/db.js
+++ server/db/db.js
@@ -1,16 +1,23 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
-const { BESSIES_DB_USER, BESSIES_DB_PASSWORD, BESSIES_DB_NAME, BESSIES_DB_PORT, BESSIES_DB_HOST, BESSIES_DB_SSL } = process.env;
+const { isDev, BESSIES_DB_USER, BESSIES_DB_PASSWORD, BESSIES_DB_NAME, BESSIES_DB_PORT, BESSIES_DB_HOST, BESSIES_DB_SSL } = process.env;
 
-const pool = new Pool({
-  user: BESSIES_DB_USER,
-  password: BESSIES_DB_PASSWORD,
-  database: BESSIES_DB_NAME,
-  port: BESSIES_DB_PORT,
-  host: BESSIES_DB_HOST,
-  ssl: BESSIES_DB_SSL,
-});
+const config = isDev ?
+  {
+    user: BESSIES_DB_USER,
+    password: BESSIES_DB_PASSWORD,
+    database: BESSIES_DB_NAME,
+    port: BESSIES_DB_PORT,
+    host: BESSIES_DB_HOST,
+    ssl: BESSIES_DB_SSL,
+  } :
+  {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  };
+
+const pool = new Pool(config);
 
 module.exports = {
   query: async (text, params) => {
